e87d36cd0d86fa0723e63d12305103d38d3f5dab
Ray Andrew <4437323+rayandrews@users.noreply.github.com>
2022-06-09 01:46:04 -0500

    remove unused codes

--- apps/backend/src/utils/test-utils.ts
+++ apps/backend/src/utils/test-utils.ts
@@ -1,13 +1,6 @@
 import {Sequelize} from 'sequelize-typescript'
 import {Umzug, SequelizeStorage} from 'umzug'
 
-// const database = new Sequelize({
-//   dialect: 'postgres',
-//   models: [path.resolve(__dirname, '../models/**/*.ts')],
-//   repositoryMode: true,
-//   logging: false,
-// })
-
 function setupMigration(database: Sequelize) {
   let umzug = new Umzug({
     migrations: {glob: 'database/migrations/*.ts'},
